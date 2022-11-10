f3e1ff95d76d4f03d7baef5382b7b8946ae0c4cc
Syrus Farris <syrious@Syruss-MBP.lan>
2022-11-10 15:35:41 -0600

    hooked up sanity to the project. the project can now fetch data from sanity.io and display it to the webpage

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from 'react';
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner} from '../components';
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    {console.log(bannerData)}
 
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Candles of man scents</p>
-      </div>
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Candles of many scents</p>
+    </div>
 
-      <div className="products-container">
-        {['Product 1', 'Product 2'].map(
-        (product) => product)}
-      </div>
+    <div className="products-container">
+      {products?.map((product) => product.name)}
+    </div>
 
-      <FooterBanner />
-    </>
-  )
+    <FooterBanner />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
