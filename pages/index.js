107485b8f1a69f75981ae81be097d5e29ccfdba0
Syrus Farris <syrious@Syruss-MBP.lan>
2022-11-09 18:46:43 -0600

    created the Hero Banner outline and did setup for implementing sanity. Also figured out some dependency errors a long the way.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner} from '../components';
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -15,7 +17,7 @@ const Home = () => {
         (product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
 }
